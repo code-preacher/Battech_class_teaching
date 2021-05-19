9a1c4d2d346f86b90cc3d536e4b176d592c07dfd
codepreacher <75647192+code-preacher@users.noreply.github.com>
2021-05-19 09:05:04 -0700

    Add files via upload

---
+++ Piracy_flexy/src/piracy_flexy/Piracy_flexy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package piracy_flexy;
+
+/**
+ *
+ * @author codepreacher
+ */
+public class Piracy_flexy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new License_check().setVisible(true);
+    }
+    
+}
