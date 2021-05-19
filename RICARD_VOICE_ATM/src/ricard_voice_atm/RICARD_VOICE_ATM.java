e3f8728a1a256bf62873662e3d30da02fd364daa
codepreacher <75647192+code-preacher@users.noreply.github.com>
2021-05-19 09:08:26 -0700

    Add files via upload

---
+++ RICARD_VOICE_ATM/src/ricard_voice_atm/RICARD_VOICE_ATM.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ricard_voice_atm;
+
+/**
+ *
+ * @author codepreacher
+ */
+public class RICARD_VOICE_ATM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Splash show =new Splash();
+        show.setVisible(true);
+        
+    }
+    
+}
